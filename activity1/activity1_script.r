691ca23c3450fe3265c205fd56fcc4b6e47a6b76
Nicholas Quinn '20 <nquinn@colgate.edu>
2020-01-24 12:10:53 -0500

    Finished Line of Code

---
+++ activity1/activity1_script.r
@@ -0,0 +1,18 @@
+# Activity 1
+print("hello R world")
+#Three Times Three
+3*3
+#Four plus Four
+4+4
+#Five divided by 1
+5/1
+
+#Working with values
+a <- 4100302034
+a*1000
+a/10
+
+#working with vectors
+v <- c(3,4,5,6)
+#R can do Vector Math!
+v*a
