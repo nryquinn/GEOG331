240391be9bdc977fb28831cb09d94535a6897903
Nick <nryquinn@gmail.com>
2020-01-29 12:14:42 -0500

    updates to Activity 2

--- Activity2 script.R
+++ Activity2 script.R
@@ -38,3 +38,97 @@ str(datW)
 ########QUESTION 1#########
 #How many rows and columns are in this dataset?#
 #There are 9 columns and 157849 rows#
+
+#specify a colun with a proper date format
+datW$dateF <- as.Date(datW$DATE, "%Y-%m-%d")
+str(datW)
+
+#create a date column by reformatting to only include years
+#ensure it as as a number
+datW$year <- as.numeric(format(datW$dateF,"%Y"))
+
+#######QUESTION 2#########
+#Using your textbook or online R resources,
+#describe the difference between character, numeric,
+#integer, and factor data. Create an example vector
+#of each data type with 5 objects in it
+
+#find out all unique site names
+levels(datW$NAME)
+
+#Mean Max temperature for aberdeen
+mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"])
+
+#Find mean with na.rm function
+mean(datW$TMAX[datW$NAME == "ABERDEEN, WA US"], na.rm = TRUE)
+
+#Calculate average daily temperature
+datW$TAVE <- datW$TMIN + ((datW$TMAX-datW$TMIN)/2)
+
+#get the mean across all sites
+#the by function is a list of one or more variables to index over.
+#FUN indicates the function we want to use
+#if you want to specify any function specific arguments use a comma and add them after the function
+#here we want to use the na.rm arguments specific to 
+averageTemp <- aggregate(datW$TAVE, by=list(datW$NAME), FUN="mean",na.rm=TRUE)
+averageTemp
+
+#Change automatic output of column names to be more meaningful
+#note that MAAT is a common abbreviation for Mean Annual Air Temperature
+colnames(averageTemp) <- c("Name", "MAAT")
+averageTemp
+
+#convert level to number for factor data type
+#you will have to reference the level output or look at the row of data to see the character designation.
+datW$siteN <- as.numeric(datW$NAME)
+
+#make a histogram for the first site in our levels
+#main= is the title name argument.
+#Here you want to paste the actual name of the factor not the numeric index
+#since that will be more meaningful. 
+hist(datW$TAVE[datW$siteN == 1],
+     freq=FALSE, 
+     main = paste(levels(datW$NAME)[1]),
+     xlab = "Average daily temperature (degrees C)", 
+     ylab="Relative frequency",
+     col="grey50",
+     border="white")
+
+##########QUESTION 3##########
+#Using help(hist) and help(paste) look up all of the arguments in my
+#hist function above. Describe what each argument is doign in the hist
+#function above
+
+#creating the same histogram but with lines for mean and sd
+hist(datW$TAVE[datW$siteN == 1],
+     freq=FALSE, 
+     main = paste(levels(datW$NAME)[1]),
+     xlab = "Average daily temperature (degrees C)", 
+     ylab="Relative frequency",
+     col="grey50",
+     border="white")
+abline(v = mean(datW$TAVE[datW$siteN ==1], na.rm =TRUE),
+       col = "tomato3",
+       lwd = 3)
+
+#Add sd line below the mean in same color and thickness
+abline(v = mean(datW$TAVE[datW$siteN == 1], na.rm = TRUE)
+       -sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE),
+       col = "tomato3",
+       lty = 3,
+       lwd = 3)
+
+#Ad sd line above
+abline(v = mean(datW$TAVE[datW$siteN == 1],na.rm=TRUE)
+       + sd(datW$TAVE[datW$siteN == 1],na.rm=TRUE), 
+       col = "tomato3", 
+       lty = 3,
+       lwd = 3)
+
+#########QUESTION 4##############
+#Make histograms for at least three other sites' daily
+#average temp (you choose the sites, but label!)
+#include SD and Mean
+#use different colors for bars, add them all into the same window
+#using par(mfrow=c(2,2)) before you run the code that makes all 4 histograms
+
