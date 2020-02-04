1e344ef8f8e7467e7520e5ab1954ebd647f59546
Nick <nryquinn@gmail.com>
2020-02-04 17:35:23 -0500

    Activity 2 Final Copy

--- Activity2 script.R
+++ Activity2 script.R
@@ -112,7 +112,7 @@ hist(datW$TAVE[datW$siteN == 1],
 
 ##########QUESTION 3###################
 #Using help(hist) and help(paste) look up all of the arguments in my
-#hist function above. Describe what each argument is doign in the hist
+#hist function above. Describe what each argument is doing in the hist
 #function above
 
 help(hist)
@@ -161,7 +161,7 @@ hist(datW$TAVE[datW$siteN == 2],
     main = paste(levels(datW$NAME)[2]),
     xlab = "Average daily temperature (degrees C)", 
     ylab="Relative frequency",
-    col="grey60",
+    col="darkorange",
     border="white")
 #Adding the Mean line
 abline(v = mean(datW$TAVE[datW$siteN ==2], na.rm =TRUE),
@@ -180,13 +180,14 @@ abline(v = mean(datW$TAVE[datW$siteN == 2],na.rm=TRUE)
        lty = 3,
        lwd = 3)
 
+
 ###Site 3###
 hist(datW$TAVE[datW$siteN == 3],
      freq=FALSE, 
      main = paste(levels(datW$NAME)[3]),
      xlab = "Average daily temperature (degrees C)", 
      ylab="Relative frequency",
-     col="blue50",
+     col="darkolivegreen",
      border="white")
 #Adding the Mean line
 abline(v = mean(datW$TAVE[datW$siteN ==3], na.rm =TRUE),
@@ -211,7 +212,7 @@ hist(datW$TAVE[datW$siteN == 4],
      main = paste(levels(datW$NAME)[4]),
      xlab = "Average daily temperature (degrees C)", 
      ylab="Relative frequency",
-     col="green50",
+     col="black",
      border="white")
 #Adding the Mean line
 abline(v = mean(datW$TAVE[datW$siteN ==4], na.rm =TRUE),
@@ -236,7 +237,7 @@ hist(datW$TAVE[datW$siteN == 5],
      main = paste(levels(datW$NAME)[5]),
      xlab = "Average daily temperature (degrees C)", 
      ylab="Relative frequency",
-     col="yellow50",
+     col="dodgerblue",
      border="white")
 #Adding the Mean line
 abline(v = mean(datW$TAVE[datW$siteN ==5], na.rm =TRUE),
@@ -279,7 +280,7 @@ y.plot <- dnorm(seq(-10,30, length.out = 100),
                 sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE))
 
 #creates a density saled to fit the plot
-#puts multiple thins on hte same plot
+#puts multiple things on the same plot
 #Maximum value is the same between the two datasets on the plot
 #creates the y coordinates
 y.scaled <- (max(h1$density)/max(y.plot)) *y.plot
@@ -297,10 +298,90 @@ points(x.plot,
 #Does daily air temperature look like it is normally
 #distributed at all sites?
 
+###Site 2###
+h2 <- hist(datW$TAVE[datW$siteN == 2],
+           freq=FALSE, 
+           main = paste(levels(datW$NAME)[2]),
+           xlab = "Average daily temperature (degrees C)", 
+           ylab="Relative frequency",
+           col="darkorange",
+           border="white")
+x.plot.Site2 <-seq(0,40, length.out = 100)
+y.plot.Site2 <- dnorm(seq(0,40, length.out = 100),
+                mean(datW$TAVE[datW$siteN == 2], na.rm = TRUE),
+                sd(datW$TAVE[datW$siteN == 2], na.rm = TRUE))
+y.scaled.Site2 <- (max(h2$density)/max(y.plot.Site2)) *y.plot.Site2
+points(x.plot.Site2,
+       y.scaled.Site2,
+       type = "l",
+       col = "royalblue3",
+       lwd = 4,
+       lty = 2)
+
+###Site 3###
+h3 <- hist(datW$TAVE[datW$siteN == 3],
+           freq=FALSE, 
+           main = paste(levels(datW$NAME)[3]),
+           xlab = "Average daily temperature (degrees C)", 
+           ylab="Relative frequency",
+           col="darkolivegreen",
+           border="white")
+x.plot.Site3 <-seq(-40,40, length.out = 100)
+y.plot.Site3 <- dnorm(seq(-40,40, length.out = 100),
+                      mean(datW$TAVE[datW$siteN == 3], na.rm = TRUE),
+                      sd(datW$TAVE[datW$siteN == 3], na.rm = TRUE))
+y.scaled.Site3 <- (max(h3$density)/max(y.plot.Site3)) *y.plot.Site3
+points(x.plot.Site3,
+       y.scaled.Site3,
+       type = "l",
+       col = "royalblue3",
+       lwd = 4,
+       lty = 2)
+
+###Site 4###
+h4 <- hist(datW$TAVE[datW$siteN == 4],
+          freq=FALSE, 
+          main = paste(levels(datW$NAME)[4]),
+          xlab = "Average daily temperature (degrees C)", 
+          ylab="Relative frequency",
+          col="black",
+          border="white")
+x.plot.Site4 <-seq(0,40, length.out = 100)
+y.plot.Site4 <- dnorm(seq(0,40, length.out = 100),
+                      mean(datW$TAVE[datW$siteN == 4], na.rm = TRUE),
+                      sd(datW$TAVE[datW$siteN == 4], na.rm = TRUE))
+y.scaled.Site4 <- (max(h4$density)/max(y.plot.Site4)) *y.plot.Site4
+points(x.plot.Site4,
+       y.scaled.Site4,
+       type = "l",
+       col = "royalblue3",
+       lwd = 4,
+       lty = 2)
+
+###Site 5###
+h5 <- hist(datW$TAVE[datW$siteN == 5],
+           freq=FALSE, 
+           main = paste(levels(datW$NAME)[5]),
+           xlab = "Average daily temperature (degrees C)", 
+           ylab="Relative frequency",
+           col="dodgerblue",
+           border="white")
+x.plot.Site5 <-seq(-30,30, length.out = 100)
+y.plot.Site5 <- dnorm(seq(-30,30, length.out = 100),
+                      mean(datW$TAVE[datW$siteN == 5], na.rm = TRUE),
+                      sd(datW$TAVE[datW$siteN == 5], na.rm = TRUE))
+y.scaled.Site5 <- (max(h5$density)/max(y.plot.Site5)) *y.plot.Site5
+points(x.plot.Site5,
+       y.scaled.Site5,
+       type = "l",
+       col = "darkorchid",
+       lwd = 4,
+       lty = 2)
+
 ###### continuing on ################
 
 help(dnorm)
-#dnorm used for probability densit
+#dnorm used for probability density
 #pnorm returns vector for probability below q
 #qnorm returns the value associated wtih a possibility
 
@@ -339,21 +420,100 @@ qnorm(0.95,
 #How often do you expect to observe temperatures greater than the
 #current threshold for extreme high temperatures?
 
+#creating a new mean temp
+new.mean = mean(datW$TAVE[datW$siteN ==1], na.rm = TRUE) + 4
+
+#finding how often
+qnorm(0.95,
+      new.mean,
+      sd(datW$TAVE[datW$siteN == 1], na.rm = TRUE))
+
+
+
 ##### QUESTION 7 ######################
 #Make a hist of daily precipitation for Aberdeen. This is an example of a data distribution
 #that isn't normal. Look up exponential, beta, and gamma distribution using the internet.
 #Look at the range of values these descriptions can describe. Would any of these
 #distributions describe the shape of daily precipitation data?
 
+#creating the histogram
+par(mfrow = c(1,1))
+hist(datW$PRCP[datW$siteN == 1],
+     freq = FALSE,
+     main = paste(levels(datW$NAME)[1]),
+     xlab = "Average Precipitation (mm)",
+     ylab = "Relative Frequency",
+     col = "grey50",
+     border = "white")
+
+
 ##### QUESTION 8 #####################
 #Use the sum function to get precipitation for each year and site in the data
 #Choose one site and make a histogram of annual precipitation. 
 #Describe the general shape of the data and whether you think it is normally distributed
 
+#creating data frame
+TotalPrecip.Year <- aggregate(datW$PRCP, by =list(datW$year, datW$NAME), FUN = "sum", na.rm = TRUE)
+colnames(TotalPrecip.Year) = c("Year", "Site Name", "Total Precipitation")
+head(TotalPrecip.Year)
+
+#creating numeric site name
+TotalPrecip.Year$siteN = as.numeric(TotalPrecip.Year$`Site Name`)
+
+#creating histogram
+hist(TotalPrecip.Year$`Total Precipitation`[TotalPrecip.Year$siteN == 1],
+     freq = FALSE,
+     main = paste(levels(TotalPrecip.Year$`Site Name`)[1]),
+     xlab = "Total Precipitation (mm)",
+     ylab = "Relative Frequency",
+     col = "grey50",
+     border = "white")
+
+#assessing normality
+hQuestion8 <- hist(TotalPrecip.Year$`Total Precipitation`[TotalPrecip.Year$siteN == 1],
+                   freq = FALSE,
+                   main = paste(levels(TotalPrecip.Year$`Site Name`)[1]),
+                   xlab = "Total Precipitation (mm)",
+                   ylab = "Relative Frequency",
+                   col = "grey50",
+                   border = "white")
+x.plot.Question8 <-seq(1000,3000, length.out = 100)
+y.plot.Question8 <- dnorm(seq(1000,3000, length.out = 100),
+                      mean(TotalPrecip.Year$`Total Precipitation`[TotalPrecip.Year$siteN == 1], na.rm = TRUE),
+                      sd(TotalPrecip.Year$`Total Precipitation`[TotalPrecip.Year$siteN == 1], na.rm = TRUE))
+y.scaled.Question8 <- (max(hQuestion8$density)/max(y.plot.Question8)) *y.plot.Question8
+points(x.plot.Question8,
+       y.scaled.Question8,
+       type = "l",
+       col = "darkorchid",
+       lwd = 4,
+       lty = 2)
+
 ##### QUESTION 9 ####################
 #Get the mean of annual precipitation for all sites. Compare to the mean
 #annual temperatures you calculated earlier. In general terms,
 #describe how the climate varies between sites.
+
+#Finding Average of Annual Precip by Site
+MeanAnnualPrecip.Site <- aggregate(TotalPrecip.Year$`Total Precipitation`, by = list(TotalPrecip.Year$`Site Name`),
+                                   FUN = "mean", na.rm = TRUE)
+colnames(MeanAnnualPrecip.Site) = c("Site Name", "Mean Annual Precipitation")
+MeanAnnualPrecip.Site
+
+#Creating Data Frame for Average Annual Temp
+MeanAnnualTemp.Year <- aggregate(datW$TAVE, by = list(datW$year, datW$NAME), FUN = "mean", na.rm = TRUE)
+colnames(MeanAnnualTemp.Year) = c("Year", "Site Name", "Average Temperature")
+head(MeanAnnualTemp.Year)
+
+#Finding Average of Annual Temp by Site
+MeanAnnualTemp.Site <- aggregate(MeanAnnualTemp.Year$`Average Temperature`, by =list(MeanAnnualTemp.Year$`Site Name`),
+                                 FUN = "mean", na.rm = TRUE)
+colnames(MeanAnnualTemp.Site) = c("Site Name", "Mean Annual Temperature")
+MeanAnnualTemp.Site
+
+#Creating a dataframe with all values across all sites
+AnnualTempandPrecip <- data.frame(MeanAnnualTemp.Site$`Site Name`, MeanAnnualTemp.Site$`Mean Annual Temperature`, MeanAnnualPrecip.Site$`Mean Annual Precipitation`)
+AnnualTempandPrecip
 
 #### QUESTION 10 ####################
 #What's the GitHub link to your script for this activity?
